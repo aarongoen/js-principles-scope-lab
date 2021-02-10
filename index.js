f4de3260f5054f1d28ae65d4dadd868d311c2271
aarongoen <aarongoen2.0@gmail.com>
2021-02-10 14:25:30 -0500

    commit tree 485044565cb9ac3ce31d443d5e6c9960306fdb2d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Write your solution in this file!f
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+
+}
+
+var bestCustomer 
+
+function setBestCustomer() {
+     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'
+}
